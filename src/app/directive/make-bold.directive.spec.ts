71ba9d118798132fb03bba0a79fe4edf1799cc70
k.shivani <127122687+shivani-radha@users.noreply.github.com>
2023-03-15 23:05:56 +0530

    Delete make-bold.directive.spec.ts

--- src/app/directive/make-bold.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MakeBoldDirective } from './make-bold.directive';
-
-describe('MakeBoldDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MakeBoldDirective();
-    expect(directive).toBeTruthy();
-  });
-});
