826eb808cbbca7455bc88f058b850ca1564cca77
k.shivani <127122687+shivani-radha@users.noreply.github.com>
2023-03-15 23:19:53 +0530

    Update dummy.component.ts

--- src/app/dummy/dummy.component.ts
+++ src/app/dummy/dummy.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dummy',
   templateUrl: './dummy.component.html',
-  styleUrls: ['./dummy.component.css']
 })
 export class DummyComponent {
 
